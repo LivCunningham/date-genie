7ab931589c514320a52ccd4109bec248c59d690f
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-06-21 11:55:48 -0400

    dkfjhgfdk

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import Filter from './pages/Filter.js'
+// import Filter from './pages/Filter.js'
 
 class Login extends Component {
   render() {
     return (
       <>
         <h1>LOGIN HERE YO</h1>
-        <Link to={`./pages/Filter`}>
-          <button className="adventure">CHOOSE YOUR ADVENTURE YO</button>
-        </Link>
+        {/* <Link to={`./pages/Filter`}> */}
+        <button className="adventure">CHOOSE YOUR ADVENTURE YO</button>
+        {/* </Link> */}
       </>
     )
   }
