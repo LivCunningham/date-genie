35c9c7f9b3bf87c13770b5a5f097a8ccf4ca6d90
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-06-22 08:10:36 -0400

    jhfgcd

--- src/pages/Filter.js
+++ src/pages/Filter.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
-import Header from '../components/Header.js'
+import { Link } from 'react-router-dom'
 
 class Filter extends Component {
   render() {
     return (
       <>
-        <Header />
+        <header className="the-top">
+          <nav className="navbar">
+            <Link className="START" to={`./pages/Filter.js`}>
+              START
+            </Link>
+            <Link className="VISITED" to={`./pages/BeenThere.js`}>
+              VISITED
+            </Link>
+            <Link className="ABOUT" to={`./pages/About.js`}>
+              ABOUT
+            </Link>
+          </nav>
+        </header>
       </>
     )
   }
